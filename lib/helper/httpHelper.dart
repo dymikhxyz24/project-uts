2e7658b42d39e92d51e1b439309af0091af4a2dd
dymikhxyz24 <dymikhxyz24@gmail.com>
2023-11-13 20:01:08 +0700

    upload

---
+++ lib/helper/httpHelper.dart
@@ -0,0 +1,33 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:http/http.dart' as http;
+
+class HttpHelper {
+  final String _urlBase = "https://fakestoreapi.com/";
+
+  Future<List?> getProducts() async {
+    var urlParse = Uri.parse(_urlBase + "products");
+    http.Response result = await http.get(urlParse);
+
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      return jsonResponse;
+    } else {
+      return [];
+    }
+  }
+
+  Future<List?> getCarts() async {
+    var urlParse = Uri.parse(_urlBase + "carts");
+
+    http.Response result = await http.get(urlParse);
+
+    if (result.statusCode == HttpStatus.ok) {
+      final jsonResponse = json.decode(result.body);
+      return jsonResponse;
+    } else {
+      return [];
+    }
+  }
+}
